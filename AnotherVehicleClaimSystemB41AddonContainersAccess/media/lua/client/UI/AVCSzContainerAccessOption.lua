d7bfba8b7f03746981bb85a644510a914f8e8791
JP Freitas <joaopedrof@icloud.com>
2023-07-04 22:51:26 -0300

    feat: mds q saco

--- AnotherVehicleClaimSystemB41AddonContainersAccess/media/lua/client/UI/AVCSzContainerAccessOption.lua
+++
@@ -1,8 +0,0 @@
-if not AVCS.oAVCSUserPermissionPanelcreateChildren then
-    AVCS.oAVCSUserPermissionPanelcreateChildren = AVCS.UI.UserPermissionPanel.createChildren
-end
-
-function AVCS.UI.UserPermissionPanel:createChildren()
-    AVCS.oAVCSUserPermissionPanelcreateChildren(self)
-    self:addSets(getText("IGUI_AVCS_User_Permissions_lblAllowContainersAccess"), "AllowContainersAccess")
-end
